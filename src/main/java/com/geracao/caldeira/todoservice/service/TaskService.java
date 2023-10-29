94872e315207c82b9efaae353d5924a71d0d210b
Lucas Gabriel <gabif.johann@gmail.com>
2023-10-29 19:18:32 -0300

    refactor: add repository structure

--- src/main/java/com/geracao/caldeira/todoservice/service/TaskService.java
+++ src/main/java/com/geracao/caldeira/todoservice/service/TaskService.java
@@ -1,43 +1,50 @@
 package com.geracao.caldeira.todoservice.service;
 
 import com.geracao.caldeira.todoservice.model.Task;
+import com.geracao.caldeira.todoservice.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class TaskService {
-    private final List<Task> taskList;
+    private final TaskRepository taskRepository;
 
-    public TaskService() {
-        taskList = new ArrayList<>();
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
     }
 
     public List<Task> getAllTasks() {
-        return taskList;
+        return taskRepository.getAllTasks();
     }
 
     public Task addTask(Task task) {
-        taskList.add(task);
-        return task;
+        return taskRepository.addNewTask(task);
     }
 
     public Task editTask(Long taskId, Task updatedTask) {
-        findTaskById(taskId).ifPresent(existingTask -> {
-            existingTask.setDescription(updatedTask.getDescription());
-            existingTask.setDueDate(updatedTask.getDueDate());
-            existingTask.setCompleted(updatedTask.isCompleted());
-        });
-        return updatedTask;
+
+        Optional<Task> existingTask = taskRepository.findById(taskId);
+        if (existingTask.isPresent()) {
+            Task taskToUpdate = existingTask.get();
+            taskToUpdate.setDescription(updatedTask.getDescription());
+            taskToUpdate.setDueDate(updatedTask.getDueDate());
+            taskToUpdate.setCompleted(updatedTask.isCompleted());
+
+            return taskRepository.addNewTask(taskToUpdate);
+        } else {
+            // Lida com o caso em que a tarefa não existe
+            return null;
+        }
     }
 
     public void deleteTask(long taskId) {
-        findTaskById(taskId).ifPresent(taskList::remove);
+        Optional<Task> taskToDelete = taskRepository.findById(taskId);
+        taskToDelete.ifPresent(taskRepository::delete);
     }
 
     public Optional<Task> findTaskById(Long taskId) {
-        return taskList.stream().filter(task -> task.getTaskId() == taskId).findFirst();
+        return taskRepository.findById(taskId);
     }
 }
