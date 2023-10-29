2715cec3c79ac604083f2f40e5d617253ecc9f38
Lucas Gabriel <gabif.johann@gmail.com>
2023-10-29 14:40:46 -0300

    first commit

---
+++ src/main/java/com/geracao/caldeira/todoservice/ToDoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.geracao.caldeira.todoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoServiceApplication.class, args);
+	}
+}
