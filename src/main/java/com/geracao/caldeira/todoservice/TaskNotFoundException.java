4cde9b2951ab112b17fda3bdd23a5f8d6e2ebcd7
Lucas Gabriel <gabif.johann@gmail.com>
2023-10-29 18:33:01 -0300

    add: exception files

--- src/main/java/com/geracao/caldeira/todoservice/TaskNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.geracao.caldeira.todoservice;
-
-public class TaskNotFoundException extends RuntimeException {
-    public TaskNotFoundException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/com/geracao/caldeira/todoservice/exception/CustomError.java
@@ -0,0 +1,8 @@
+package com.geracao.caldeira.todoservice.exception;
+
+import lombok.Data;
+
+@Data
+public class CustomError {
+    private String message;
+}
